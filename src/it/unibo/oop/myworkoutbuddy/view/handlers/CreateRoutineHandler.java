6f43c6818ed9f5ba69e0f6bb0693c5a7d7dc5cfe
Nicola Piscaglia <nicola.piscaglia2@studio.unibo.it>
2016-02-08 01:40:11 +0100

    created new StyleSingleton class

--- src/it/unibo/oop/myworkoutbuddy/view/handlers/CreateRoutineHandler.java
+++ src/it/unibo/oop/myworkoutbuddy/view/handlers/CreateRoutineHandler.java
@@ -1,9 +1,22 @@
 package it.unibo.oop.myworkoutbuddy.view.handlers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * 
- *Handler of the CreateRoutineView. It collects user routine.
+ * Handler of the CreateRoutineView. It collects user routine.
  */
 public class CreateRoutineHandler {
+
+    @FXML
+    private Button btnInsertExercise;
+
+    @FXML
+    private AnchorPane anchorEx;
+
+    @FXML
+    private Button btnSaveRoutine;
 
 }
