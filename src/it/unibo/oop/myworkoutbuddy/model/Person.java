e71ddd27f4be1f77be7f9d740ff382253fb6967b
Lorenzo Pacini <lorenzo.pacini@studio.unibo.it>
2016-02-18 23:40:34 +0100

    added new class Person

---
+++ src/it/unibo/oop/myworkoutbuddy/model/Person.java
@@ -0,0 +1,28 @@
+package it.unibo.oop.myworkoutbuddy.model;
+/**
+ * 
+ * User's general data.
+ * -------------------------------------------------------------
+ */
+public interface Person {
+    /**
+     * 
+     * @return the Name of User
+     */
+    String getFirstName();
+    /**
+     * 
+     * @return the Surname of User
+     */
+    String getLastName();
+    /**
+     * 
+     * @return the birthDate of User
+     */
+    int getAge();
+    /**
+     * 
+     * @return the email of the User
+     */
+    String getEmail();
+}
---
+++ src/it/unibo/oop/myworkoutbuddy/model/PersonImpl.java
@@ -0,0 +1,55 @@
+package it.unibo.oop.myworkoutbuddy.model;
+
+/**
+ * 
+ * User's general data.
+ * -------------------------------------------------------------
+ */
+ 
+public class PersonImpl implements Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+
+    /**
+     * 
+     * @param firstName String
+     * @param lastName String
+     * @param email String
+     * @param age int
+     */
+    public PersonImpl(final String firstName, final String lastName, final int age, final String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    @Override
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String getEmail() {
+        return this.email;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonImpl [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", email=" + email
+                + "]";
+    }
+}
