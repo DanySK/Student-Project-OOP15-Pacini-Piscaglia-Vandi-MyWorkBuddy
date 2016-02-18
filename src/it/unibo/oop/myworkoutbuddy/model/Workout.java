36064f639fac2f761205f9379db3713307e6286e
Lorenzo Pacini <lorenzo.pacini@studio.unibo.it>
2016-02-18 23:28:10 +0100

    modified

--- src/it/unibo/oop/myworkoutbuddy/model/Workout.java
+++ src/it/unibo/oop/myworkoutbuddy/model/Workout.java
@@ -3,6 +3,10 @@ package it.unibo.oop.myworkoutbuddy.model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Map;
+
+import it.unibo.oop.myworkoutbuddy.model.Body.BodyPart;
+
 /**
  * 
  * User's data of a single training session. (WorkOut)
@@ -10,6 +14,12 @@ import java.util.List;
  */
  
 public interface Workout {
+
+    /**
+     * 
+     * @param state boolean
+     */
+    void modifyState(final boolean state);
     /**
      * 
      * @return data of Workout
@@ -29,7 +39,7 @@ public interface Workout {
      * 
      * @return training card of an exercise
      */
-    WorkoutRoutine getCard();
+    WorkoutRoutine getRoutine();
 
     /**
      * 
@@ -43,4 +53,28 @@ public interface Workout {
      * @return all the scores got for an exercise
      */
     List<Integer> getScoreList();
+    /**
+     * 
+     * @return average of normalized scores
+     */
+    Double getWorkoutScore();
+
+    /**
+     * 
+     * @return a map of bodyPart and the percentage for all human muscles
+     */
+    Map<BodyPart, Double> getPercentuageParts();
+
+    /**
+     * 
+     * @return a map of bodyPart and the time of training for all human muscles
+     */
+    // Integer a dx di ,
+    Map<BodyPart, Double> getTimeParts();
+
+    /**
+     * 
+     * @return a map of GymTool codes and relatives numbers of time used
+     */
+    Map<String, Double> getTimeTools();
 }
