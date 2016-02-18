df67bd2dcba90bb9ba91a2e137082afc7582f39a
Lorenzo Pacini <lorenzo.pacini@studio.unibo.it>
2016-02-18 23:26:28 +0100

    modified

--- src/it/unibo/oop/myworkoutbuddy/model/MyWorkoutBuddyModel.java
+++ src/it/unibo/oop/myworkoutbuddy/model/MyWorkoutBuddyModel.java
@@ -1,14 +1,25 @@
 package it.unibo.oop.myworkoutbuddy.model;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-import it.unibo.oop.myworkoutbuddy.model.User.Account;
+import it.unibo.oop.myworkoutbuddy.model.Body.BodyData;
+import it.unibo.oop.myworkoutbuddy.model.Body.BodyPart;
+import it.unibo.oop.myworkoutbuddy.model.Body.BodyZone;
 
 /**
  * 
  * 
  */
 public interface MyWorkoutBuddyModel {
+    /**
+     * 
+     * @param index int
+     * @return an account at index of the Account list
+     */
+    Account getAccount(final int index);
+
     /**
      * login of User : set current login User.
      * @param userName String
@@ -20,35 +31,104 @@ public interface MyWorkoutBuddyModel {
      * 
      * @return current login User, in Optional form
      */
-    Optional<User> getLoginUser();
+    User getLoginUser();
 
     /**
      * Add a new Account.
-     * @param name String
-     * @param password String
+     * @param account Account
+     * @throws ExistentAccount 
      */
-    void addAccount(final String name, final String password);
+    void addAccount(final Account account) throws ExistentAccount;
+
+    /**
+     * 
+     * @param gymTool GymTool
+     */
+    void addGymTool(final GymTool gymTool);
 
     /**
      * Add a new User.
      * @param account Account
      * @param userData UserData
-     * @param email String
      */
-    void addUser(final Account account, final UserData userData, final String email);
+    void addUser(final Account account, final Person userData);
 
     /**
-     * Add a new GymTool.
-     * @param toolName String
-     * @param fileImage String
-     * @param vMin int
-     * @param vMax int 
-     * @param numMax int
+     * 
+     * @param userData UserData
      */
-    void addGymTool(final String toolName, final String fileImage, final int vMin, final int vMax, final int numMax);
+    void addUser(Person userData);
 
     /**
-     * test method for example data.
+     * 
+     * @param code String
+     * @return current login User, in Optional form
      */
-    void testData1();
+    Optional<GymTool> getGymTool(final String code);
+
+    /**
+     * 
+     * @return the list of GymTool in an application
+     */
+    List<GymTool> getGymToolList();
+
+    /**
+     * 
+     * @return the list of Users
+     */
+    List<User> getUserList();
+
+    /**
+     * 
+     * @return true if exists the current user
+     */
+    boolean isCurrentUser();
+
+    /**
+     * 
+     * @return a list of current user's body measures
+     */
+    List<BodyData> getMeasureList();
+
+    /**
+     * 
+     * @return list of performance score for the current User
+     */
+    List<Double> performanceScore();
+
+    /**
+     * 
+     * @return map of performanceBodyPart for the current User
+     */
+    Map<BodyPart, Double> performanceBodyPart();
+
+    /**
+     * 
+     * @return map of performanceBodyZone for the current User
+     */
+    Map<BodyZone, Double> performanceBodyZone();
+
+    /**
+     * 
+     * @return map of timeBodyPart for the current User
+     */
+    Map<BodyPart, Double> timeBodyPart();
+
+    /**
+     * 
+     * @return map of timeBodyZone for the current User
+     */
+    Map<BodyZone, Double> timeBodyZone();
+
+    /**
+     * 
+     * @return map of timeGymTool for the current User
+     */
+    Map<String, Double> timeGymTool();
+
+    /**
+     * 
+     * @return list of trendBodyMass for the current User
+     */
+    List<Double> trendBodyMass();
 }
